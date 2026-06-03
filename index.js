460411de865c5725fb293457e7dcd359d5e78e2c
VVS IPHONE <vvsiphone@VVSs-MacBook-Air.local>
2026-06-03 02:32:25 -0400

    Add Render port listener

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 require('dotenv').config();
 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Puffy is alive!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
 const {
   Client,
   GatewayIntentBits,
